28ef5d6cd0352fec1e7defe842dd5e013ebf900a
William Bezuidenhout <william.bezuidenhout+github@gmail.com>
2021-01-01 19:11:06 +0200

    make exec script a little more run friendly

--- exec.sh
+++ exec.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+if [[ ! -d venv ]]; then
+    echo "First run. Creating virtualenv"
+    python3 -m venv venv
+    venv/bin/pip install -r requirements.txt
+fi
+
+echo "Running playbook for WiFi Access point setup"
 venv/bin/ansible-playbook -i inventory.yml -k run.yml
